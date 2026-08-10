8ea99ed83a42320557e43d6aa8fffbf33371ab4b
Gabriel Jose dee Oliveira <gabdev.pro@gmail.com>
2026-08-10 09:17:37 -0300

    refactor: rewrite flag test using t.Run

--- pkg/cpu/flags_test.go
+++ pkg/cpu/flags_test.go
@@ -7,46 +7,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSetFlagsToTrue(t *testing.T) {
-	flags := &cpu.Flags{}
+func TestFlags(t *testing.T) {
+	t.Run("set flags to true", func(t *testing.T) {
+		flags := &cpu.Flags{}
 
-	flags.Set(cpu.Zero, true)
-	flags.Set(cpu.Sub, true)
-	flags.Set(cpu.HalfCarry, true)
-	flags.Set(cpu.Carry, true)
+		flags.Set(cpu.Zero, true)
+		flags.Set(cpu.Sub, true)
+		flags.Set(cpu.HalfCarry, true)
+		flags.Set(cpu.Carry, true)
 
-	assert.Equal(t, true, flags.Get(cpu.Zero), "cpu.Zero flag is not set")
-	assert.Equal(t, true, flags.Get(cpu.Sub), "cpu.Sub flag is not set")
-	assert.Equal(t, true, flags.Get(cpu.HalfCarry), "cpu.HalfCarry flag is not set")
-	assert.Equal(t, true, flags.Get(cpu.Carry), "cpu.Carry flag is not set")
-}
+		assert.Equal(t, true, flags.Get(cpu.Zero), "cpu.Zero flag is not set")
+		assert.Equal(t, true, flags.Get(cpu.Sub), "cpu.Sub flag is not set")
+		assert.Equal(t, true, flags.Get(cpu.HalfCarry), "cpu.HalfCarry flag is not set")
+		assert.Equal(t, true, flags.Get(cpu.Carry), "cpu.Carry flag is not set")
+	})
 
-func TestSetFlagsToFalse(t *testing.T) {
-	flags := &cpu.Flags{}
+	t.Run("set flags to false", func(t *testing.T) {
+		flags := &cpu.Flags{}
 
-	flags.Set(cpu.Zero, false)
-	flags.Set(cpu.Sub, false)
-	flags.Set(cpu.HalfCarry, false)
-	flags.Set(cpu.Carry, false)
+		flags.Set(cpu.Zero, false)
+		flags.Set(cpu.Sub, false)
+		flags.Set(cpu.HalfCarry, false)
+		flags.Set(cpu.Carry, false)
 
-	assert.Equal(t, false, flags.Get(cpu.Zero), "cpu.Zero flag is set")
-	assert.Equal(t, false, flags.Get(cpu.Sub), "cpu.Sub flag is set")
-	assert.Equal(t, false, flags.Get(cpu.HalfCarry), "cpu.HalfCarry flag is set")
-	assert.Equal(t, false, flags.Get(cpu.Carry), "cpu.Carry flag is set")
-}
+		assert.Equal(t, false, flags.Get(cpu.Zero), "cpu.Zero flag is set")
+		assert.Equal(t, false, flags.Get(cpu.Sub), "cpu.Sub flag is set")
+		assert.Equal(t, false, flags.Get(cpu.HalfCarry), "cpu.HalfCarry flag is set")
+		assert.Equal(t, false, flags.Get(cpu.Carry), "cpu.Carry flag is set")
+	})
 
-func TestSetFlagsToMixedValues(t *testing.T) {
-	flags := &cpu.Flags{}
+	t.Run("set flags to mixed values", func(t *testing.T) {
+		flags := &cpu.Flags{}
 
-	flags.Set(cpu.Zero, false)
-	flags.Set(cpu.Sub, true)
-	flags.Set(cpu.HalfCarry, false)
-	flags.Set(cpu.Carry, true)
+		flags.Set(cpu.Zero, false)
+		flags.Set(cpu.Sub, true)
+		flags.Set(cpu.HalfCarry, false)
+		flags.Set(cpu.Carry, true)
 
-	assert.Equal(t, false, flags.Get(cpu.Zero), "cpu.Zero flag is set")
-	assert.Equal(t, true, flags.Get(cpu.Sub), "cpu.Sub flag is not set")
-	assert.Equal(t, false, flags.Get(cpu.HalfCarry), "cpu.HalfCarry flag is set")
-	assert.Equal(t, true, flags.Get(cpu.Carry), "cpu.Carry flag is not set")
+		assert.Equal(t, false, flags.Get(cpu.Zero), "cpu.Zero flag is set")
+		assert.Equal(t, true, flags.Get(cpu.Sub), "cpu.Sub flag is not set")
+		assert.Equal(t, false, flags.Get(cpu.HalfCarry), "cpu.HalfCarry flag is set")
+		assert.Equal(t, true, flags.Get(cpu.Carry), "cpu.Carry flag is not set")
+	})
 }
 
 func TestValue(t *testing.T) {
